bece64880e8bbfa0feecf0fdc904083bbd589fc4
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-01-06 03:52:14 -0500

    * added rudimentary version of `providers2`

--- R/providers.R
+++ R/providers.R
@@ -10,26 +10,34 @@
 #'    * [Provider Enrollment API](https://data.cms.gov/provider-characteristics/medicare-provider-supplier-enrollment/medicare-fee-for-service-public-provider-enrollment)
 #'    * [Provider Enrollment Data Dictionary](https://data.cms.gov/resources/medicare-fee-for-service-public-provider-enrollment-data-dictionary)
 #'
-#'
 #' @param npi `<chr>` 10-digit Individual NPI
+#'
 #' @param pac `<chr>` 10-digit PECOS Associate Control (PAC) ID
+#'
 #' @param enid `<chr>` 15-digit Medicare Enrollment ID
-# @param specialty `<chr>` Enrollment specialty code __OR__ description
+#'
+#' @param spec_code `<chr>` Enrollment specialty code
+#'
+#' @param spec_desc `<chr>` Enrollment specialty description
+#'
 #' @param state `<chr>` Enrollment state abbreviation
+#'
 #' @param first,middle,last `<chr>` Individual provider's name
+#'
 #' @param org `<chr>` Organizational provider's name
+#'
 #' @param gender `<chr>` Individual provider's gender:
 #'
 #'    * `"F"` (Female)
 #'    * `"M"` (Male)
 #'    * `"9"` (Unknown/Organization)
 #'
-#' @return `<tibble>` of search results
+#' @returns `<tibble>` of search results
 #'
 #' @examples
 #' providers2(enid = "I20040309000221")
 #'
-#' # providers2(npi = "1417918293", specialty = "14-41")
+#' providers2(npi = "1417918293", spec_code = "14-41")
 #'
 #' providers2(pac = "2860305554", gender = "9")
 #'
@@ -39,7 +47,8 @@
 providers2 <- function(npi       = NULL,
                        pac       = NULL,
                        enid      = NULL,
-                       # specialty = NULL,
+                       spec_code = NULL,
+                       spec_desc = NULL,
                        first     = NULL,
                        middle    = NULL,
                        last      = NULL,
@@ -47,16 +56,41 @@ providers2 <- function(npi       = NULL,
                        state     = NULL,
                        gender    = NULL) {
 
-  rlang::list2(
+  args <- rlang::list2(
     "NPI"                = npi,
     "PECOS_ASCT_CNTL_ID" = pac,
     "ENRLMT_ID"          = enid,
-    # "PROVIDER_TYPE_CD"   = specialty_code,
-    # "PROVIDER_TYPE_DESC" = specialty_description,
+    "PROVIDER_TYPE_CD"   = spec_code,
+    "PROVIDER_TYPE_DESC" = spec_desc,
     "STATE_CD"           = state,
     "FIRST_NAME"         = first,
     "MDL_NAME"           = middle,
     "LAST_NAME"          = last,
     "ORG_NAME"           = org,
-    "GNDR_SW"            = gender)
+    "GNDR_SW"            = gender) |>
+    purrr::compact()
+
+  url <- public_filter(
+    endpoint = "dataset",
+    title = "Medicare Fee-For-Service  Public Provider Enrollment")[["identifier"]] |>
+    httr2::request() |>
+    httr2::req_url_query(!!!format_query(args), size = 5000)
+
+  stats <- url |>
+  httr2::req_url_path_append("stats") |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+
+  # offset_sequence(stats$data$found_rows)
+
+  resp <- url |> httr2::req_perform() |>
+    httr2::resp_body_string() |>
+    RcppSimdJson::fparse()
+
+  collapse::qTBL(resp[["data"]]) |>
+    setNames(c("NPI", "PECOS_ASCT_CNTL_ID", "ENRLMT_ID",
+               "PROVIDER_TYPE_CD", "PROVIDER_TYPE_DESC",
+               "STATE_CD", "FIRST_NAME", "MDL_NAME",
+               "LAST_NAME", "ORG_NAME", "GNDR_SW"))
+
 }
