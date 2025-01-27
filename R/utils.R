0eb4f30e0d943fa646ecc09900bf1cfdb3a01040
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-01-27 13:17:37 -0500

    * corrected `offset_sequence()` behavior

--- R/utils.R
+++ R/utils.R
@@ -7,9 +7,9 @@
 #'
 #' @param start `<int>` Offset start; either `0` (default) or `limit`
 #'
-#' @returns      `<int>` If `n <= limit`, simply returns `n`. If `n > limit`,
-#'                       an integer sequence is returned, beginning at `start`
-#'                       and of length equal to `n / limit`.
+#' @returns `<int>` If `n <= limit`, simply returns `n`. If `n > limit`, an
+#'                  integer sequence is returned, beginning at `start` and of
+#'                  length equal to `n / limit`.
 #'
 #' @examples
 #' offset_sequence(100, 10)
@@ -31,18 +31,19 @@ offset_sequence <- \(n, limit, start = 0) {
 
   if (n <= limit) return(n)
 
-  seq_(
-    from = start,
-    to   = ifelse(
-      n %% limit == 0,
-      n,
-      sum(n, limit)),
-    by   = limit)
+  seq_(from = start, to = n, by = limit)
 }
 
+# Incorrect
+# seq_(from = start, to = ifelse(n %% limit == 0, n, sum(n, limit)), by = limit)
 # seq_(from = 0, to = sum(100, 10), by = 10)
 # seq_(from = 0, to = sum(47984, 5000), by = 5000)
 # seq_(from = 0, to = sum(47984, 2000), by = 2000)
+
+# Correct
+# seq_(from = 0, to = 100, by = 10)
+# seq_(from = 0, to = 47984, by = 5000)
+# seq_(from = 0, to = 47984, by = 2000)
 
 #' Flatten Column
 #' @param i `<list>` list to flatten
