7ac9d9e4de3193bee9f41b7a8e49ebf62a750abc
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-02-19 07:00:51 -0500

    * Quick draft for NLM's NPI Registry API

---
+++ R/nppes.R
@@ -0,0 +1,67 @@
+#' Search the NLM NPI Registry
+#' @param terms `<chr>` Search terms, separated by spaces
+#' @returns `<tibble>` of search results
+#' @examples
+#' nppes_nlm("john bethesda")
+#'
+#' nppes_nlm("Dentist Valdosta")
+#'
+#' # Not returning all results
+#' nppes_nlm("Valdosta")
+#' @autoglobal
+#' @export
+nppes_nlm <- function(terms) {
+
+  req <- request("https://clinicaltables.nlm.nih.gov/api/npi_idv/v3/search?") |>
+    req_url_query(
+      terms   = terms,
+      maxList = 500,
+      count   = 500,
+      offset  = 0L,
+      .space = "form"
+    )
+
+  resp <- req_perform(req) |> resp_simple_json()
+
+  n <- resp[[1]]
+
+  if (n > 7500L) {
+    cli::cli_abort(c(
+      "Your search returned {.strong {.val {n}}} results.",
+      "x" = "The NLM API limit is {.strong {.emph 7,500}}."))
+  }
+
+  results <- resp[[4]] |>
+    as.data.frame() |>
+    set_names(c("name", "npi", "specialty", "address")) |>
+    as_tbl()
+
+  nreq <- offset_length(n, 500) > 1
+
+  cli_n_results_requests(n, 500)
+
+  if (false(nreq)) {
+
+    return(results)
+
+    } else {
+
+      results2 <- req_perform_iterative(
+      req,
+      next_req        = iterate_with_offset(
+        param_name    = "offset",
+        start         = 500,
+        offset        = 500,
+        resp_complete = is_complete_with_limit(500))) |>
+      map(
+        function(x) {
+          x <- resp_simple_json(x)
+          x[[4]] |>
+            as.data.frame() |>
+            set_names(c("name", "npi", "specialty", "address"))
+        }) |>
+      rowbind()
+
+    as_tbl(rowbind(results, results2))
+  }
+}
