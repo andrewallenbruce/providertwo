8f2591c5bab50b881629617bea0b38af70f94720
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-04-02 20:22:59 -0400

    * `open_dashboard()`

---
+++ R/open_dashboard.R
@@ -0,0 +1,87 @@
+#' Open Dashboard Response Class
+#'
+#' @param response `<list>` or `<openRespDash>` object returned from the API call to the dashboard.
+#'
+#' @returns An S7 `<openRespDash>` object.
+#'
+#' @examples
+#' open_dashboard()
+#' @autoglobal
+#' @name OpenResp
+#' @export
+
+#' @autoglobal
+#' @rdname OpenResp
+#' @export
+openRespDash <- new_class(
+  name = "openRespDash",
+  package = NULL,
+  properties = list(response = class_list)
+)
+
+#' @autoglobal
+#' @rdname OpenResp
+#' @export
+open_dashboard <- function() {
+  openRespDash(
+    response = openCurr("dashboard") |>
+      prop("identifier") |>
+      request() |>
+      req_url_query(
+        schema  = "false",
+        keys    = "true",
+        results = "true",
+        count   = "true",
+        offset  = 0L,
+        limit   = 500L
+      ) |>
+      req_perform() |>
+      resp_body_string() |>
+      fparse() |>
+      _[["results"]]
+  )
+}
+
+# x |>
+#   fastplyr::f_arrange(data_metrics, dashboard_row_number) |>
+#   fcompute(
+#     id = collapse::groupid(dashboard_row_number),
+#     grp = collapse::groupid(data_metrics),
+#     metric = data_metrics
+#   ) |>
+#   print(n = Inf)
+
+#   x |>
+#     # fastplyr::f_arrange(data_metrics, dashboard_row_number)
+#     fcompute(
+#       id = collapse::groupid(dashboard_row_number),
+#       metric = data_metrics) |>
+#     print(n = Inf)
+# }
+#
+# dashboard |>
+#   as_tbl() |>
+#   f_select(Data_Metrics, Total) |>
+#   f_arrange(-Total) |>
+#   print(n = Inf)
+#
+# dashboard |>
+#   as_tbl() |>
+#   f_select(Data_Metrics, Total) |>
+#   f_arrange(-Total) |>
+#   print(n = Inf)
+#
+# dash <- dashboard |>
+#   as_tbl() |>
+#   slt(-Dashboard_Row_Number) |>
+#   pivot(ids = 1) |>
+#   roworder(Data_Metrics) |>
+#   mtt(year = delist(stri_extract_all_regex(variable, "[0-9]{4}")))
+#
+# dash |>
+#   sbt(na(year), metrics = Data_Metrics, amount = value)
+#
+# dash |>
+#   sbt(not_na(year), year, metrics = Data_Metrics, amount = value) |>
+#   mtt(year = as_int(year)) |>
+#   rsplit(~ metrics)
