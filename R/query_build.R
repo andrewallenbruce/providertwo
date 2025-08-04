e9d36b20be5f063a9181493e5bb588717bb8dfdb
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-08-04 15:24:37 -0400

    * all methods for query building

--- R/query_build.R
+++ R/query_build.R
@@ -34,28 +34,35 @@ params_years <- function(obj, qry) {
 #' @returns A list of query parameters matched to an endpoint's fields.
 #'
 #' @examples
-#' qry <- new_query(
-#'    first_name  = starts_with("An"),
-#'    middle_name = ends_with("e"),
-#'    last_name   = contains("J"),
-#'    state       = any_of(c("CA", "GA", "NY")),
-#'    year        = 2021:2025,
-#'    practice_state_or_us_territory = any_of(c("CA", "GA", "NY")),
-#'    allowed_charges = greater_than(1000))
+#' build(
+#'   endpoint("drug_state"),
+#'   new_query(
+#'     year = 2022:2024,
+#'     state = any_of(c("CA", "GA", "NY"))))
 #'
-#' build(endpoint("enroll_prov"), qry)
+#' build(
+#'   endpoint("enroll_prov"),
+#'   new_query(
+#'     first_name = starts_with("And"),
+#'     last_name = ends_with("ce")))
 #'
-#' build(endpoint("dial_facility"), qry)
+#' build(
+#'   endpoint("dial_facility"),
+#'   new_query(city = any_of("BIRMINGHAM")))
 #'
-#' build(endpoint("quality_payment"), qry)
+#' build(
+#'   endpoint("quality_payment"),
+#'   new_query(
+#'     year = 2017:2025,
+#'     practice_state_or_us_territory = any_of(c("GA", "FL")),
+#'     allowed_charges = less_than(10000)))
 #'
-#' build(endpoint("pdc_clinician"), qry)
-#'
-#' build(endpoint("grp_cover_nature"), qry)
-#'
-#' build(collection("hha"), qry)
-#'
-#' build(collection("hhc"), qry)
+#' build(
+#'   endpoint("pdc_clinician"),
+#'   new_query(
+#'     provider_first_name = starts_with("An"),
+#'     provider_last_name = contains("J"),
+#'     state = any_of(c("CA", "GA", "NY"))))
 #'
 #' @autoglobal
 #' @export
@@ -73,42 +80,73 @@ S7::method(build, list(class_catalog, class_query)) <- function(obj, qry) {
 
 S7::method(build, list(class_current, class_query)) <- function(obj, qry) {
 
-  params <- params_fmt(query_match(obj, qry))
-
-  list(
-    title      = obj@metadata$title,
-    dimensions = obj@dimensions,
-    identifier = obj@identifier,
-    params     = params
-  )
-}
-
-S7::method(build, list(class_temporal, class_query)) <- function(obj, qry) {
-
-  id     <- params_years(obj, qry)
-  params <- params_fmt(query_match(obj, qry))
-
-  list(
-    title      = obj@metadata$title,
-    dimensions = obj@dimensions,
-    identifier = id,
-    params     = params
-  )
-}
-
-S7::method(build, list(care_current, class_query)) <- function(obj, qry) {
-
-  pr <- params_fmt(query_match(obj, qry), is_care = TRUE)
+  pr <- query_match(obj, qry)
+  pr <- params_fmt(pr)
   id <- params_flatten(obj@identifier, pr)
-  dm <- get_elem(S7::props(obj@dimensions), "rows|limit", regex = TRUE)
+
+  x  <- `if`(
+    is.null(pr),
+    0L,
+    perform_simple(request(id)) |> _[["count"]]
+    )
 
   class_results(
     title  = obj@metadata$title,
     params = names(pr),
     base   = id,
-    total  = dm$rows,
-    found  = `if`(is.null(pr), 0L, get_elem(perform_simple(req_url_path_append(request(id), "stats")), "found_rows")),
-    limit  = 5000L
+    total  = obj@dimensions@rows,
+    found  = x,
+    limit  = obj@dimensions@limit
+  )
+}
+
+S7::method(build, list(class_temporal, class_query)) <- function(obj, qry) {
+
+  id <- params_years(obj, qry)
+  pr <- query_match(obj, qry)
+  pr <- params_fmt(pr)
+  id <- map(id, function(x) params_flatten(x, pr))
+
+  res <- map(id, request) |>
+    req_perform_parallel(on_error = "continue") |>
+    map(function(x) fparse(resp_body_string(x)) |>
+          _[["count"]])
+
+  class_results(
+    title  = obj@metadata$title,
+    params = names(pr),
+    base   = id,
+    total  = obj@dimensions@rows,
+    found  = unlist(res, use.names = FALSE),
+    limit  = obj@dimensions@limit
+  )
+}
+
+S7::method(build, list(care_current, class_query)) <- function(obj, qry) {
+
+  pr <- query_match(obj, qry)
+  pr <- params_fmt(pr, is_care = TRUE)
+  id <- params_flatten(obj@identifier, pr)
+
+  x  <- `if`(
+    is.null(pr),
+    0L,
+    get_elem(
+      perform_simple(
+        req_url_path_append(
+          request(id),
+          "stats")
+        ),
+      "found_rows")
+    )
+
+  class_results(
+    title  = obj@metadata$title,
+    params = names(pr),
+    base   = id,
+    total  = obj@dimensions@rows,
+    found  = x,
+    limit  = obj@dimensions@limit
   )
 }
 
