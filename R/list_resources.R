5304cc10a764f4f30949e24c5bcb94763d60ec35
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-08-03 23:49:15 -0400

    * fixed bug in list_resources

--- R/list_resources.R
+++ R/list_resources.R
@@ -19,50 +19,35 @@ list_resources <- new_generic("list_resources", "obj", function(obj) {
 #' @noRd
 tidy_resources <- function(x) {
   x |>
-    fcompute(
+    collapse::mtt(
       year     = extract_year(name),
+      year     = ifelse(is.na(year), extract_year(downloadURL), year),
       file     = rm_space(gremove(name, " [0-9]{4}|[0-9]{4} ")),
       size     = fs::as_fs_bytes(fileSize),
       ext      = tolower(fs::path_ext(downloadURL)),
-      download = downloadURL
-    ) |>
+      download = downloadURL,
+      .keep = c("year", "file", "size", "ext")) |>
     ffill(year) |>
     roworder(-year, ext, -size) |>
     as_fibble()
 }
 
 method(list_resources, class_group) <- function(obj) {
-  prop(obj, "members") |>
-    map(list_resources)
+  prop(obj, "members") |> map(list_resources)
 }
 
 method(list_resources, class_catalog) <- function(obj) {
-  cli::cli_alert_warning(
-    paste0(
-      "{.fn list_resources} needs ",
-      "a {.cls class_care} ",
-      "object, not {.obj_type_friendly {obj}}."))
-  invisible(NULL)
-}
-
-
-method(list_resources, class_endpoint) <- function(obj) {
-  cli::cli_alert_warning(
-    paste0(
-      "{.fn list_resources} needs ",
-      "a {.cls care_current} or {.cls care_temporal} ",
-      "object, not {.obj_type_friendly {obj}}."))
-  invisible(NULL)
+  cli::cli_alert_warning(c("Skipping {.obj_type_friendly {obj}}"))
+  return()
 }
 
 method(list_resources, class_care) <- function(obj) {
-  prop(obj, "access") |>
-    list_resources()
+  prop(obj, "access") |> list_resources()
 }
 
 method(list_resources, care_current) <- function(obj) {
   prop(obj, "metadata") |>
-    _$resources |>
+    get_elem("resources") |>
     map(request) |>
     req_perform_parallel(on_error = "continue") |>
     resps_successes() |>
@@ -78,7 +63,6 @@ method(list_resources, care_temporal) <- function(obj) {
     map(request) |>
     req_perform_parallel(on_error = "continue") |>
     resps_successes() |>
-    resps_data(function(resp)
-      parse_string(resp, query = "/data")) |>
+    resps_data(function(resp) parse_string(resp, query = "/data")) |>
     tidy_resources()
 }
