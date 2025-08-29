a754a43781d8ccf9ba2f82c16a8ef363b77dd2c9
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-08-29 18:41:20 -0400

    * Medicaid API no longer wants indexed multi-queries, leave index blank

--- R/query_generate.R
+++ R/query_generate.R
@@ -86,9 +86,36 @@ query_default <- function(args) {
 
 #' @autoglobal
 #' @noRd
+query_default2 <- function(args) {
+
+  purrr::imap(args, function(x, N) {
+
+    V <- if (is_modifier(x)) x@value else unlist(x, use.names = FALSE)
+    O <- if (is_modifier(x)) tolower(gsub("_", "+", x@operator, fixed = TRUE)) else "="
+    N <- gsub(" ", "+", N, fixed = TRUE)
+
+    c(paste0("conditions[<<i>>][property]=", N),
+      paste0("conditions[<<i>>][operator]=", O),
+      `if`(
+        length(V) > 1L,
+        paste0("conditions[<<i>>][value][]=", V),
+        paste0("conditions[<<i>>][value]=", V)))
+  }) |>
+    unname() |>
+    purrr::imap(function(x, idx)
+      gsub(x           = x,
+           pattern     = "<<i>>",
+           replacement = idx - 1,
+           fixed       = TRUE)
+    ) |>
+    purrr::map(paste0, collapse = "&")
+}
+
+#' @autoglobal
+#' @noRd
 generate_query <- function(x = NULL, is_care = FALSE) {
   if (is_null(x) || is_empty(x)) return(NULL)
-  set_names(`if`(is_care, query_care(x), query_default(x)), names2(x))
+  set_names(`if`(is_care, query_care(x), query_default2(x)), names2(x))
 }
 
 #' @autoglobal
